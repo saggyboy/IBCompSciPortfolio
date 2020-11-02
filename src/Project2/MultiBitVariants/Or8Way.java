3ef4ee622515d9c3420af95cd14f813e3318cf49
saggyboy <andrewseybold@gmail.com>
2020-11-02 16:05:48 -0700

    2 better solutions to 8way

--- src/Project2/MultiBitVariants/Or8Way.java
+++ src/Project2/MultiBitVariants/Or8Way.java
@@ -5,29 +5,47 @@ import Project2.ElementaryLogicGates.Or;
 
 public class Or8Way {
     public boolean out;
-    private Or[] ors = new Or[4];
-    private Or or5 = new Or();
-    private Or or6 = new Or();
+    private Or[] ors = new Or[6];
     private Or or7 = new Or();
 
-    public Or8Way(){
-        for (int i = 0; i < 4; i++) {
+    public Or8Way() {
+        for (int i = 0; i < 6; i++) {
             ors[i] = new Or();
         }
     }
 
-    public void compute(boolean[] in){
-        boolean[] result1 = new boolean[4];
-        for (int i = 0; i < 3; i++) {
-            boolean prev = in[i];
-            boolean curr = in[i+1];
-            ors[i].compute(curr, prev);
-            result1[i] = ors[i].out;
+    public void compute(boolean[] in) {
+        boolean prev;
+        boolean curr;
+        boolean[] results1 = new boolean[4];
+        for (int i = 0; i < 8; i += 2) {
+            prev = in[i];
+            curr = in[i + 1];
+            ors[i / 2].compute(curr, prev);
+            results1[i / 2] = ors[i / 2].out;
         }
-        or5.compute(result1[0], result1[1]);
-        or6.compute(result1[2],result1[3]);
-        or7.compute(or5.out, or6.out);
+
+        boolean[] results2 = new boolean[2];
+        for (int i = 0; i < 4; i += 2) {
+            prev = results1[i];
+            curr = results1[i + 1];
+            ors[i / 2].compute(curr, prev);
+            results2[i / 2] = ors[i / 2].out;
+        }
+
+        or7.compute(results2[0], results2[1]);
         out = or7.out;
     }
+
+
+    private void compute2(boolean[] in) {
+        Or thing = new Or();
+        out = false;
+        for (int i = 0; i < in.length; i++) {
+            thing.compute(out, in[i]);
+            out = thing.out;
+       }
+    }
+
 
 }
