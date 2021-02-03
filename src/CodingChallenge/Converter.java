c23169cda17dbcfdc2ee45601e9d654aed3b7ea2
saggyboy <andrewseybold@gmail.com>
2021-02-02 21:00:38 -0700

    Update of convert comment

--- src/CodingChallenge/Converter.java
+++ src/CodingChallenge/Converter.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Converter {
     public static String convert(int dayOfWeek){
-        //Totally arbitrary way of doing this. If's work just as well
+        //Totally arbitrary way of doing this but visually clean. If statements work just as well.
         Map<Integer, String> days = new HashMap<>();
         days.put(0, "Monday");
         days.put(1, "Tuesday");
