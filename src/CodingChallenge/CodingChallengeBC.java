2888fdad4972c1792095dc7d57acb702e90c7729
saggyboy <andrewseybold@gmail.com>
2021-02-08 15:12:57 -0700

    Finished all parts

--- src/CodingChallenge/CodingChallengeBC.java
+++ src/CodingChallenge/CodingChallengeBC.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class CodingChallengeBC {
     public static void main(String[] args){
         //b.
-        List<Integer> passengers = Arrays.asList(212, 454, 342, 170, 632, 462, 505, 234, 654, 736);
+        List<Integer> passengers = Arrays.asList(300, 100, 100, 100, 200, 400, 450, 450, 500, 550);
         Iterator<Integer> iterator = passengers.listIterator();
         int[] p_array = new int[passengers.size()];
         for (int i = 0; i < passengers.size(); i++) {
@@ -16,17 +16,30 @@ public class CodingChallengeBC {
         }
 
         //c.
-        int mostPassengers = p_array[0];
-        int mostPassengersIndex = 0;
+        int[] averagesPerDay = new int[7];
         for (int i = 0; i < p_array.length; i++) {
-            int curr = p_array[i];
+            averagesPerDay[i%7] += p_array[i];
+        }
+
+        int mostPassengers = 0;
+        int mostPassengersIndex = 0;
+        for (int i = 0; i < 7; i++) {
+            int curr = averagesPerDay[i];
+
+            if (i < (p_array.length%7)){
+                curr /= (p_array.length/7 + 1);
+            } else {
+                curr /= (p_array.length/7);
+            }
+            System.out.println(curr);
+
             if (curr > mostPassengers){
                 mostPassengers = curr;
                 mostPassengersIndex = i;
             }
         }
         //Find day number in any week
-        int dayWithinWeek = mostPassengersIndex%7;
+        int dayWithinWeek = mostPassengersIndex;
         String dayWithMostPassengers = Converter.convert(dayWithinWeek);
         System.out.println(dayWithMostPassengers);
     }
